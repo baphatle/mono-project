2f52635b30940a5e693f6be22d8db9cc63475b57
baphatle <baphatledev@gmail.com>
2024-01-20 13:36:17 +0700

    Fix create new post err

--- view/src/components/Form.js
+++ view/src/components/Form.js
@@ -25,23 +25,33 @@ export default function Form() {
             const { post } = response.data.data
             const author = { _id: post.author, name: user.userName }
             dispatch({ type: "CREATE_ONE_POST", payload: { ...post, author, isEditable: true } })
-            setPostInput({content: ""})
+            setPostInput({ content: "" })
         } catch (error) {
             setErrorMessage(error.response.data.message)
-        }   
+        }
     }
     return (
         <section className="form-section">
-            <form className="form" onSubmit={onSubmitHandle}>
-                {errorMessage && (
-                    <div className="error-message">Error: {errorMessage}</div>
-                )}
-                <textarea type="text" name="content" id="content" class="content"
-                    placeholder="What's happening?" value={postInput.content} onChange = {(e) => {
-                        setPostInput({ ...postInput, [e.target.name]: e.target.value })
-                      }} ></textarea>
-                <button className="btn" type="submit">Post</button>
-            </form>
+            {user ? (
+                <>
+                    <form className="form" onSubmit={onSubmitHandle}>
+                        {errorMessage && (
+                            <div className="error-message">Error: {errorMessage}</div>
+                        )}
+                        <textarea type="text" name="content" id="content" class="content"
+                            placeholder="What's happening?" value={postInput.content} onChange={(e) => {
+                                setPostInput({ ...postInput, [e.target.name]: e.target.value })
+                            }} ></textarea>
+                        <button className="btn" type="submit">Post</button>
+                    </form>
+                </>
+            ) : (
+                <>
+                    <h3>All Posts:</h3>
+                </>
+            )}
         </section>
     )
 }
+
+
